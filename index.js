0317b2c98049689bd1e6b556a7dde23e62fba747
salamantalel <nmoinkett@gmail.com>
2025-06-11 08:43:23 +0300

    Add object lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+// Initial employee object
+const employee = {
+  name: "Sal",
+  streetAddress: "123 Code Lane"
+};
+
+// Function 1: Non-destructive update
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+
+// Function 2: Destructive update
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// Function 3: Non-destructive delete
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// Function 4: Destructive delete
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
